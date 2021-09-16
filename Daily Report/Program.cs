2863d351bce27b583d26605e2954cf8284687270
DForeman <dlydellf@hotmail.com>
2021-09-16 16:13:56 -0500

    Program prints 1st required question, saves & displays answer

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -10,7 +10,12 @@ namespace Daily_Report
     {
         static void Main()
         {
-            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+            Console.WriteLine("The Tech Academy\nStudent Daily Report\n\n"); // "Daily Report's" displayed headings
+            Console.WriteLine("What is your name?\n"); // 1st required question
+            string name = Console.ReadLine(); // 1st question's answer assigned to a descriptive variable & saved according to its proper data type
+
+            // Displays all entered data, if User desires:
+            Console.WriteLine("Your name is " + name);
             Console.ReadLine();
         }
     }
