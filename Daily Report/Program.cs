4913c0df656df322da624fbc23bcecd9936274d2
DForeman <dlydellf@hotmail.com>
2021-09-16 16:16:39 -0500

    Program prints 2nd required question; saves & displays answer

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -13,9 +13,12 @@ namespace Daily_Report
             Console.WriteLine("The Tech Academy\nStudent Daily Report\n\n"); // "Daily Report's" displayed headings
             Console.WriteLine("What is your name?\n"); // 1st required question
             string name = Console.ReadLine(); // 1st question's answer assigned to a descriptive variable & saved according to its proper data type
+            Console.WriteLine("What course are you on?\n"); // 2nd required question
+            string courseName = Console.ReadLine(); // 2nd question's answer assigned to a descriptive variable & saved according to its proper data type
 
             // Displays all entered data, if User desires:
             Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your current course of study is: " + courseName);
             Console.ReadLine();
         }
     }
