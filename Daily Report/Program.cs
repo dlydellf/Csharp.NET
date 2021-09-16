3a084ad5a7fcece916e91b5c0e9bf1428b3926a7
DForeman <dlydellf@hotmail.com>
2021-09-16 15:49:13 -0500

    Program prints 1st requirement - g2g

---
+++ Daily Report/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+        }
+    }
+}
