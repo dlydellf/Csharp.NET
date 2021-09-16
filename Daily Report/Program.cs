084ece658a059f7a003a3da9585bf63470176fe8
DForeman <dlydellf@hotmail.com>
2021-09-16 16:36:51 -0500

    Program prints 4th question; converts/saves/displays answerProgram prints 4t required question; saves & displays answer

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -12,17 +12,21 @@ namespace Daily_Report
         {
             Console.WriteLine("The Tech Academy\nStudent Daily Report\n\n"); // "Daily Report's" displayed headings
             Console.WriteLine("What is your name?"); // 1st required question
-            string name = Console.ReadLine(); // 1st question's answer assigned to a descriptive variable & saved according to its proper data type
+            string name = Console.ReadLine(); // Assigns 1st question's answer to a variable; saves according to its proper data type
             Console.WriteLine("\nWhat course are you on?"); // 2nd required question
-            string courseName = Console.ReadLine(); // 2nd question's answer assigned to a descriptive variable & saved according to its proper data type
+            string courseName = Console.ReadLine(); // Assigns 2nd question's answer to a variable; saves according to its proper data type
             Console.WriteLine("\nWhat page number?"); // 3rd required question (can only return a STRING)
-            string pageNum = Console.ReadLine(); // 3rd question's STRING answer assigned to a temp variable for converting
-            int pageNumber = Convert.ToInt32(pageNum); // Converting STRING to an INTEGER for saving
+            string pageNum = Console.ReadLine(); // Assigns 3rd question's STRING answer to a TEMP variable for converting...
+            int pageNumber = Convert.ToInt32(pageNum); // Converts 3rd's string -> INTEGER
+            Console.WriteLine("\nDo you need help with anything?\nPlease answer \"true\" or \"false\"."); // 4th required question (can only return a STRING)
+            string trueFalse = Console.ReadLine(); // Assigns 4th question's STRING answer to a TEMP variable for converting...
+            bool needHelp = Convert.ToBoolean(trueFalse); // Converts 4th's string -> BOOLEAN
 
             // Displays all entered data, if User desires:
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Course: " + courseName);
             Console.WriteLine("Page: " + pageNumber);
+            Console.WriteLine("Need Help (T/F)? " + needHelp);
             Console.ReadLine();
         }
     }
