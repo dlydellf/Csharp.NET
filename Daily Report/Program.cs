8f1f184e72a40d8a3ad348bfe80a5d0d660ec83b
DForeman <dlydellf@hotmail.com>
2021-09-16 17:46:51 -0500

    Program prints 7th required question; saves & displays answer.  Added misc comments

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Daily_Report
 {
     class Program
     {
-        static void Main()
+        static void Main() // Program's entry point; 'static' = Main() can be called w/o an object; 'void' = Main() doesn't return anything
         {
             Console.WriteLine("The Tech Academy\nStudent Daily Report\n\n"); // "Daily Report's" displayed headings
             Console.WriteLine("What is your name?"); // 1st required question
@@ -25,15 +21,23 @@ namespace Daily_Report
             string experiences = Console.ReadLine(); // Assigns 5th question's answer to a variable; saves according to its proper data type
             Console.WriteLine("\nIs there any other feedback you'd like to provide?  Please be specific."); // 6th required question
             string feedback = Console.ReadLine(); // Assigns 6th question's answer to a variable; saves according to its proper data type
+            Console.WriteLine("\nHow many hours did you study today?"); // 7th required question (leaving as a string, since answer can vary widely)
+            string hrsStudied = Console.ReadLine(); // Assigns 7th question's answer to a variable; saves according to its proper data type
 
-            // Displays all entered data, if User desires:
+            Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine(); // Keeps window open until User closes it
+
+            /*
+            // Displays all entered data for Instructor's review:
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Course: " + courseName);
             Console.WriteLine("Page: " + pageNumber);
             Console.WriteLine("Need Help (T/F)? " + needHelp);
             Console.WriteLine("Positive Experiences -> " + experiences);
             Console.WriteLine("Additional Feedback -> " + feedback);
+            Console.WriteLine("Hours Studied: " + hrsStudied);
             Console.ReadLine();
+            */
         }
     }
 }
