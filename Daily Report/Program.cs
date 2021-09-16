c6ed8252c2d84535fb2947e7362ac8102edfebfa
DForeman <dlydellf@hotmail.com>
2021-09-16 16:36:51 -0500

    Program prints 4t required question; saves & displays answer

--- Daily Report/Program.cs
+++ Daily Report/Program.cs
@@ -18,11 +18,14 @@ namespace Daily_Report
             Console.WriteLine("\nWhat page number?"); // 3rd required question (can only return a STRING)
             string pageNum = Console.ReadLine(); // 3rd question's STRING answer assigned to a temp variable for converting
             int pageNumber = Convert.ToInt32(pageNum); // Converting STRING to an INTEGER for saving
+            Console.WriteLine("\nDo you need help with anything?"); // 4th required question
+            string needHelp = Console.ReadLine(); // 4th question's answer assigned to a descriptive variable & saved according to its proper data type
 
             // Displays all entered data, if User desires:
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Course: " + courseName);
             Console.WriteLine("Page: " + pageNumber);
+            Console.WriteLine("Need Help?" + needHelp);
             Console.ReadLine();
         }
     }
