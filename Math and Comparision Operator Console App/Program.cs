d339d58e702683c46350b8d60eac3952ee123420
DForeman <dlydellf@hotmail.com>
2021-09-17 18:29:52 -0500

    'Person 2' prints, along w/both required questions - g2g

--- Math and Comparision Operator Console App/Program.cs
+++ Math and Comparision Operator Console App/Program.cs
@@ -9,12 +9,20 @@ namespace Math_and_Comparision_Operator_Console_App
             Console.WriteLine("Anonymous Income Comparison Program"); // this is program's 1st required output
             Console.WriteLine("\nPerson 1"); // program's 2nd required output
             Console.WriteLine("What is your hourly rate? (xxx.xx)"); // program's 1st required question for 'Person 1'
-            string usersHourlyRate = Console.ReadLine(); // User's returned input will ALWAYS be a string; assigning the value to a variable for conversion...
-            decimal hourlyRate = Convert.ToDecimal(usersHourlyRate); // converting User's string to more-specific 'decimal' data type
+            string user1HourlyRate = Console.ReadLine(); // Person1's returned input will ALWAYS be a string; assigning the value to a variable for conversion...
+            decimal hourlyRate1 = Convert.ToDecimal(user1HourlyRate); // converting Person1's string to more-specific 'decimal' data type
             Console.WriteLine("How many hours do you work per week? (xx.xx)"); // program's 2nd required question for 'Person 1'
-            string usersWeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
-            decimal weeklyHours = Convert.ToDecimal(usersWeeklyHours); // converting User's string to 'decimal' data type
+            string user1WeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
+            decimal weeklyHours1 = Convert.ToDecimal(user1WeeklyHours); // converting Person1's string to 'decimal' data type
 
+            Console.WriteLine("\nPerson 2"); // program's 3rd required output
+            Console.WriteLine("What is your hourly rate? (xxx.xx)"); // program's 1st required question for 'Person 2'
+            string user2HourlyRate = Console.ReadLine(); // Person2's returned input will ALWAYS be a string; assigning the value to a variable for conversion...
+            decimal hourlyRate2 = Convert.ToDecimal(user2HourlyRate); // converting Person2's string to more-specific 'decimal' data type
+            Console.WriteLine("How many hours do you work per week? (xx.xx)"); // program's 2nd required question for 'Person 2'
+            string user2WeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
+            decimal weeklyHours2 = Convert.ToDecimal(user2WeeklyHours); // converting Person2's string to 'decimal' data type
+            
             Console.ReadLine();
 
         }
