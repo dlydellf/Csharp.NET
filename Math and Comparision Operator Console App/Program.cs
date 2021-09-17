ae1f5342efe38b7e4dbfbabd60c7685929e173af
DForeman <dlydellf@hotmail.com>
2021-09-17 18:39:57 -0500

    Program properly calculates & displays Person 1's annual salary - g2g

--- Math and Comparision Operator Console App/Program.cs
+++ Math and Comparision Operator Console App/Program.cs
@@ -22,6 +22,12 @@ namespace Math_and_Comparision_Operator_Console_App
             Console.WriteLine("How many hours do you work per week? (xx.xx)"); // program's 2nd required question for 'Person 2'
             string user2WeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
             decimal weeklyHours2 = Convert.ToDecimal(user2WeeklyHours); // converting Person2's string to 'decimal' data type
+
+            /* This code block calculates annual salaries:
+             [(hourlyRate x hoursWorkedPerWeek) = weeklySalary] x 52 weeks = annual salary
+            */
+            decimal annualSalary1 = (hourlyRate1 * weeklyHours1) * 52;
+            Console.WriteLine($"Annual salary of Person 1:\n{annualSalary1}");
             
             Console.ReadLine();
 
