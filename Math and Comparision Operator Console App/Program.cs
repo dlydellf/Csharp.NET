2a547de18ee9370ddf4429d1764ea5582681660f
DForeman <dlydellf@hotmail.com>
2021-09-17 18:01:26 -0500

    Program fulfills 1st requirement - g2g

---
+++ Math and Comparision Operator Console App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Math_and_Comparision_Operator_Console_App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+        }
+    }
+}
