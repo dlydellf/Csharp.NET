b76ca8a9939cde4a8a24cbcf3cd1b2d030ad44e3
DForeman <dlydellf@hotmail.com>
2021-09-17 18:25:36 -0500

    'Person 1' prints, along w/both required questions - g2g

--- Math and Comparision Operator Console App/Program.cs
+++ Math and Comparision Operator Console App/Program.cs
@@ -6,7 +6,15 @@ namespace Math_and_Comparision_Operator_Console_App
     {
         static void Main()
         {
-            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Anonymous Income Comparison Program"); // this is program's 1st required output
+            Console.WriteLine("\nPerson 1"); // program's 2nd required output
+            Console.WriteLine("What is your hourly rate? (xxx.xx)"); // program's 1st required question for 'Person 1'
+            string usersHourlyRate = Console.ReadLine(); // User's returned input will ALWAYS be a string; assigning the value to a variable for conversion...
+            decimal hourlyRate = Convert.ToDecimal(usersHourlyRate); // converting User's string to more-specific 'decimal' data type
+            Console.WriteLine("How many hours do you work per week? (xx.xx)"); // program's 2nd required question for 'Person 1'
+            string usersWeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
+            decimal weeklyHours = Convert.ToDecimal(usersWeeklyHours); // converting User's string to 'decimal' data type
+
             Console.ReadLine();
 
         }
