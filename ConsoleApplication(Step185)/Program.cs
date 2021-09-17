ab124133cde7482a00edc797d2bce19ce2f0ca4d
DForeman <dlydellf@hotmail.com>
2021-09-17 17:26:42 -0500

    App performs 4th requirement - g2g

--- ConsoleApplication(Step185)/Program.cs
+++ ConsoleApplication(Step185)/Program.cs
@@ -7,16 +7,16 @@ namespace ConsoleApplication_Step185_
         static void Main()
         {
             Console.WriteLine("Hi!\n1st Question - What number would you like multiplied by 50?"); // the 1st requirement
-            string userInput = Console.ReadLine(); // User's input can ONLY be a string; assigning to a variable, but needs conversion...
-            int input = Convert.ToInt32(userInput); // converting User's string -> int (whole numbers from -2 billion to 2 billion)
-            int answer = input * 50;// Stores (userInput x 50)
-            Console.WriteLine($"Your input of {userInput}, multiplied by 50, equals...{answer}\n(Press \"Enter\" to continue, please)");
+            string userInput = Console.ReadLine(); // User's input can ONLY be a string; is assigned to a variable for conversion...
+            int multiplicand = Convert.ToInt32(userInput); // converting User's string -> int (whole numbers from -2 billion to 2 billion)
+            int product = multiplicand * 50;// storing (userInput x 50)
+            Console.WriteLine($"Your input of {userInput}, multiplied by 50, equals...{product}\n(Press \"Enter\" to continue, please)");
             Console.ReadLine();
 
             Console.WriteLine("\n2nd Question - What number would you like 25 added to?"); // the 2nd requirement
-            string usersInput = Console.ReadLine(); // this will always be a string; assigned to a new variable for another conversion...
+            string usersInput = Console.ReadLine(); // this will always be a string; assigning it to a new variable for another conversion...
             int addend = Convert.ToInt32(usersInput); // converting User's string -> int
-            int total = addend + 25;// Stores (userInput + 25)
+            int total = addend + 25;// storing (userInput + 25)
             Console.WriteLine($"Your input of {usersInput}, increased by 25, is...{total}\n(\"Enter\" again, please)");
             Console.ReadLine();
 
@@ -27,6 +27,12 @@ namespace ConsoleApplication_Step185_
             Console.WriteLine($"Your input of {usersDividend}, divided by 12.5, is...\n{quotient}\n(\"Enter\"... press it)");
             Console.ReadLine();
 
+            Console.WriteLine("\n4th Question - Choose a number, and we'll check whether or not it's greater than 50"); // the 4th requirement
+            string usersChoice = Console.ReadLine(); // this variable is a string that will be converted...
+            int userChoice = Convert.ToInt32(usersChoice); // converting usersChoice -> int
+            bool answer = userChoice > 50;  // comparing usersChoice to 50
+            Console.WriteLine($"\"Your choice of {usersChoice} is greater than 50\"... this statement is: {answer}\n(One more \"Enter\" keypress to go!)");
+            Console.ReadLine();
         }
     }
 }
