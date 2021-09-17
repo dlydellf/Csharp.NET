29c2b549cd9419ad4b2f59eb0ab29de174da45dc
DForeman <dlydellf@hotmail.com>
2021-09-17 18:50:04 -0500

    Program properly calculates & displays Person 2's annual salary - g2g

--- Math and Comparision Operator Console App/Program.cs
+++ Math and Comparision Operator Console App/Program.cs
@@ -11,7 +11,7 @@ namespace Math_and_Comparision_Operator_Console_App
             Console.WriteLine("What is your hourly rate? (xxx.xx)"); // program's 1st required question for 'Person 1'
             string user1HourlyRate = Console.ReadLine(); // Person1's returned input will ALWAYS be a string; assigning the value to a variable for conversion...
             decimal hourlyRate1 = Convert.ToDecimal(user1HourlyRate); // converting Person1's string to more-specific 'decimal' data type
-            Console.WriteLine("How many hours do you work per week? (xx.xx)"); // program's 2nd required question for 'Person 1'
+            Console.WriteLine("How many hours do you work per week? (xx)"); // program's 2nd required question for 'Person 1'
             string user1WeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
             decimal weeklyHours1 = Convert.ToDecimal(user1WeeklyHours); // converting Person1's string to 'decimal' data type
 
@@ -19,7 +19,7 @@ namespace Math_and_Comparision_Operator_Console_App
             Console.WriteLine("What is your hourly rate? (xxx.xx)"); // program's 1st required question for 'Person 2'
             string user2HourlyRate = Console.ReadLine(); // Person2's returned input will ALWAYS be a string; assigning the value to a variable for conversion...
             decimal hourlyRate2 = Convert.ToDecimal(user2HourlyRate); // converting Person2's string to more-specific 'decimal' data type
-            Console.WriteLine("How many hours do you work per week? (xx.xx)"); // program's 2nd required question for 'Person 2'
+            Console.WriteLine("How many hours do you work per week? (xx)"); // program's 2nd required question for 'Person 2'
             string user2WeeklyHours = Console.ReadLine(); // returned string assigned to a variable for converting...
             decimal weeklyHours2 = Convert.ToDecimal(user2WeeklyHours); // converting Person2's string to 'decimal' data type
 
@@ -27,8 +27,11 @@ namespace Math_and_Comparision_Operator_Console_App
              [(hourlyRate x hoursWorkedPerWeek) = weeklySalary] x 52 weeks = annual salary
             */
             decimal annualSalary1 = (hourlyRate1 * weeklyHours1) * 52;
-            Console.WriteLine($"Annual salary of Person 1:\n{annualSalary1}");
-            
+            Console.WriteLine($"\nAnnual salary of Person 1:\n{annualSalary1}");
+            Console.ReadLine();
+            decimal annualSalary2 = (hourlyRate2 * weeklyHours2) * 52;
+            Console.WriteLine($"\nAnnual salary of Person 2:\n{annualSalary2}");
+
             Console.ReadLine();
 
         }
