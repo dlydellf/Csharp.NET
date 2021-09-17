ab8c17a1404c46c672633b989cd0719668999737
DForeman <dlydellf@hotmail.com>
2021-09-17 14:55:43 -0500

    App performs 2nd requirement - g2g

--- ConsoleApplication(Step185)/Program.cs
+++ ConsoleApplication(Step185)/Program.cs
@@ -10,7 +10,14 @@ namespace ConsoleApplication_Step185_
             string userInput = Console.ReadLine(); // User's input can ONLY be a string; assigning to a variable, but needs conversion...
             int input = Convert.ToInt32(userInput); // converting User's string -> int (whole numbers from -2 billion to 2 billion)
             int answer = input * 50;// Stores (userInput x 50)
-            Console.WriteLine($"Your input of {userInput}, multiplied by 50, equals...\n{answer}");
+            Console.WriteLine($"Your input of {userInput}, multiplied by 50, equals...{answer}\n(Press \"Enter\" to continue, please)");
+            Console.ReadLine();
+
+            Console.WriteLine("\n2nd Question - What number would you like 25 added to?"); // the 2nd requirement
+            string usersInput = Console.ReadLine(); // this will always be a string; assigned to a new variable for another conversion...
+            int addend = Convert.ToInt32(usersInput); // converting User's string -> int
+            int total = addend + 25;// Stores (userInput + 25)
+            Console.WriteLine($"Your input of {usersInput}, increased by 25, is...{total}\n(\"Enter\" again, please)");
             Console.ReadLine();
         }
     }
