8efc63e4d65bdc19d514dbf3f13e83738b2c9dab
DForeman <dlydellf@hotmail.com>
2021-09-17 17:50:50 -0500

    Clarified what types of numbers (whole vs decimals) to chose from

--- ConsoleApplication(Step185)/Program.cs
+++ ConsoleApplication(Step185)/Program.cs
@@ -6,14 +6,14 @@ namespace ConsoleApplication_Step185_
     {
         static void Main()
         {
-            Console.WriteLine("Hi!\n1st Question - What number would you like multiplied by 50?"); // the 1st requirement
+            Console.WriteLine("Hi!\n1st Question - What whole number would you like multiplied by 50?"); // the 1st requirement
             string userInput = Console.ReadLine(); // User's input can ONLY be a string; is assigned to a variable for conversion...
             int multiplicand = Convert.ToInt32(userInput); // converting User's string -> int (whole numbers from -2 billion to 2 billion)
             int product = multiplicand * 50;// storing (userInput x 50)
             Console.WriteLine($"Your input of {userInput}, multiplied by 50, equals... {product}\n(Press \"Enter\" to continue, please)");
             Console.ReadLine();
 
-            Console.WriteLine("\n2nd Question - What number would you like 25 added to?"); // the 2nd requirement
+            Console.WriteLine("\n2nd Question - What whole number would you like 25 added to?"); // the 2nd requirement
             string usersInput = Console.ReadLine(); // this will always be a string; assigning it to a new variable for another conversion...
             int addend = Convert.ToInt32(usersInput); // converting User's string -> int
             int total = addend + 25;// storing (userInput + 25)
@@ -27,7 +27,7 @@ namespace ConsoleApplication_Step185_
             Console.WriteLine($"Your input of {usersDividend}, divided by 12.5, is... {quotient}\n(\"Enter\" - press it)");
             Console.ReadLine();
 
-            Console.WriteLine("\n4th Question - Choose a number, and we'll check whether or not it's greater than 50"); // the 4th requirement
+            Console.WriteLine("\n4th Question - Choose a whole number, and we'll check whether or not it's greater than 50"); // the 4th requirement
             string usersChoice = Console.ReadLine(); // this variable is a string that will be converted...
             int userChoice = Convert.ToInt32(usersChoice); // converting usersChoice -> int
             bool answer = userChoice > 50;  // comparing usersChoice to 50
