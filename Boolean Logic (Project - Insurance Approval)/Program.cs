ea6a40074b2ed0c0d09a203ee0ad5cfb9d7db0d2
DForeman <dlydellf@hotmail.com>
2021-09-17 22:36:34 -0500

    1st required question's input saves & redisplays properly - g2g

---
+++ Boolean Logic (Project - Insurance Approval)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Boolean_Logic__Project___Insurance_Approval_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?"); // program's 1st required question
+            string usersAge = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> int' conversion...
+            int age = Convert.ToInt32(usersAge); // converting this number to a more appropriate data type
+
+            Console.ReadLine();
+        }
+    }
+}
