1632402fa00fc72383a158656a57172509041132
DForeman <dlydellf@hotmail.com>
2021-09-17 22:48:30 -0500

    3rd required question's input saves & redisplays properly - g2g

--- Boolean Logic (Project - Insurance Approval)/Program.cs
+++ Boolean Logic (Project - Insurance Approval)/Program.cs
@@ -8,11 +8,16 @@ namespace Boolean_Logic__Project___Insurance_Approval_
         {
             Console.WriteLine("What is your age?"); // program's 1st required question
             string usersAge = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> int' conversion...
-            int age = Convert.ToInt32(usersAge); // converting this number to a more appropriate data type
+            int age = Convert.ToInt32(usersAge); // converting the string to a more appropriate data type
 
             Console.WriteLine("(Please answer with either \"True\" or \"False\")\nHave you ever had a DUI?"); // program's 2nd required question
             string anyDUIs = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> boolean' conversion...
             bool DUIs = Convert.ToBoolean(anyDUIs); // converting the string its more accurate data type
+
+            Console.WriteLine("How many speeding tickets do you have?"); // program's 3rd required question
+            string usersTickets = Console.ReadLine(); // saving User's input to a variable for another 'string -> int' conversion
+            int tickets = Convert.ToInt32(usersTickets); // coverting the string to a more suitable data type
+            Console.WriteLine($"you claim to have had {tickets} tickets - wow...");
 
             Console.ReadLine();
         }
