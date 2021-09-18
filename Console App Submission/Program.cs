9d6516470728f8b88fbc9ead822cb6ce57cb0b52
DForeman <dlydellf@hotmail.com>
2021-09-18 16:49:08 -0500

    Program fulfills 2nd requirement (do while loop)

--- Console App Submission/Program.cs
+++ Console App Submission/Program.cs
@@ -8,9 +8,15 @@ namespace Console_App_Submission
         static void Main()
         {
             int index = 20; // starting value of 20 assigned to int variable
+
+            do // This do-while loop runs at least once, changing the font color
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            } while (index % 2 != 0);
+
             while (index > -1) // boolean condition: while index is greater than -1...
             {
-                Console.WriteLine($"{index}"); // ...display index's value, 
+                Console.WriteLine($"Counting down from: {index}..."); // ...display index's value, 
                 --index; // ...and decrement index by 1
             }
             Console.ReadLine(); // Window stays open to view result
