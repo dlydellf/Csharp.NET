2f7dca47cf413a5a80c6943631edf1be14d47607
DForeman <dlydellf@hotmail.com>
2021-09-18 13:10:43 -0500

    Package's weight displays & saves properly

--- Branching Price Quote App/Program.cs
+++ Branching Price Quote App/Program.cs
@@ -6,7 +6,10 @@ namespace Branching_Price_Quote_App
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below."); // program's 1st required statement
+            Console.WriteLine("Please enter the package weight:"); // User's 1st required prompt
+            int weight = Convert.ToInt32(Console.ReadLine());// User's input returns a 'string'; this line converts it to 'int'
+
             Console.ReadLine();
         }
     }
