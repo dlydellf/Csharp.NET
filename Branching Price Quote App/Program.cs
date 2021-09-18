21f4a0d4cd35dd1da8cd544325cbddf380fb05b6
DForeman <dlydellf@hotmail.com>
2021-09-18 12:48:11 -0500

    "Price Quote" program displays 1st requirement properly

---
+++ Branching Price Quote App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Branching_Price_Quote_App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+        }
+    }
+}
