f86bcd90bac3abaa3320218f82c3e9d3bfaed612
DForeman <dlydellf@hotmail.com>
2021-09-18 13:15:12 -0500

    Added 'weight restriction' condition & error message

--- Branching Price Quote App/Program.cs
+++ Branching Price Quote App/Program.cs
@@ -9,6 +9,10 @@ namespace Branching_Price_Quote_App
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below."); // program's 1st required statement
             Console.WriteLine("Please enter the package weight:"); // User's 1st required prompt
             int weight = Convert.ToInt32(Console.ReadLine());// User's input returns a 'string'; this line converts it to 'int'
+            if (weight > 50)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day."); // if weight > 50, this error displays and program exits
+            }
 
             Console.ReadLine();
         }
