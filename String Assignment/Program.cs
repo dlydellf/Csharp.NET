d270bc4f312bfe197c39f09d4953082651c81f21
DForeman <dlydellf@hotmail.com>
2021-09-18 17:23:17 -0500

    Converted a string to uppercase - g2g

--- String Assignment/Program.cs
+++ String Assignment/Program.cs
@@ -11,8 +11,14 @@ namespace String_Assignment
             string middle = "a concate"; // string 2 of 3
             string end = "nated sentence... trust me."; // string 3 of 3
 
-            Console.WriteLine($"{start}{middle}{end}");
+            Console.WriteLine($"{start}{middle}{end}"); // concatenating the above
             Console.ReadLine();
+
+            //toUpperCase():
+            string before = "we were small letters, but now we're uppercase!"; // string to convert to uppercase
+            string after = before.ToUpper(); // the conversion
+            Console.WriteLine(after); // displays the result
+            Console.ReadLine(); // keeps console opened to view result
         }
     }
 }
