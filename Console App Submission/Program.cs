f9021875331a3e99200e085f213bd03d660fbae6
DForeman <dlydellf@hotmail.com>
2021-09-18 16:25:51 -0500

    Program fulfills 1st requirement

---
+++ Console App Submission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Console_App_Submission
+{
+    class Program
+    {
+        // 1st Requirement: This while loop counts down from 20 to 0:
+        static void Main()
+        {
+            int index = 20; // starting value of 20 assigned to int variable
+            while (index > -1) // condition: while index is greater than -1...
+            {
+                Console.WriteLine($"{index}"); // ...display index's value
+                --index; // ...and decrement index by 1
+            }
+            Console.ReadLine(); // Window stays open to view result
+        }
+
+    }
+}
