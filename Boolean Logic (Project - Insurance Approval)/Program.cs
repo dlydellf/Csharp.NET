92103c3aa60813b550349fd739f57e986a1da1d3
DForeman <dlydellf@hotmail.com>
2021-09-17 23:05:21 -0500

    "Insurance Approval" program correctly calculates using boolean logic - g2g

--- Boolean Logic (Project - Insurance Approval)/Program.cs
+++ Boolean Logic (Project - Insurance Approval)/Program.cs
@@ -8,18 +8,22 @@ namespace Boolean_Logic__Project___Insurance_Approval_
         {
             Console.WriteLine("What is your age?"); // program's 1st required question
             string usersAge = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> int' conversion...
-            int age = Convert.ToInt32(usersAge); // converting the string to a more appropriate data type
+            int age = Convert.ToInt32(usersAge); // casting the string to a more appropriate data type
 
             Console.WriteLine("(Please answer with either \"True\" or \"False\")\nHave you ever had a DUI?"); // program's 2nd required question
             string anyDUIs = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> boolean' conversion...
-            bool DUIs = Convert.ToBoolean(anyDUIs); // converting the string its more accurate data type
+            bool DUIs = Convert.ToBoolean(anyDUIs); // casting the string its more accurate data type
 
             Console.WriteLine("How many speeding tickets do you have?"); // program's 3rd required question
             string usersTickets = Console.ReadLine(); // saving User's input to a variable for another 'string -> int' conversion
-            int tickets = Convert.ToInt32(usersTickets); // coverting the string to a more suitable data type
-            Console.WriteLine($"you claim to have had {tickets} tickets - wow...");
+            int tickets = Convert.ToInt32(usersTickets); // casting the string to a more suitable data type
 
-            Console.ReadLine();
+            /* Boolean logic is applied to three combined
+             * rules to determine User's insurance qualification */
+            bool qualified = age > 15 && DUIs == false && tickets <= 3;// Qualified if: 1) 16 or older, 2) has no DUIs, & 3) tickets =< 3
+            Console.WriteLine($"Qualified?\n{qualified}");
+
+            Console.ReadLine(); // window must be closed by User
         }
     }
 }
