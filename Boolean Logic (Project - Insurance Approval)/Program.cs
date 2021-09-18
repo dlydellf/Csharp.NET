a03ffe268e767845548e3ed9eb55d78333486f98
DForeman <dlydellf@hotmail.com>
2021-09-17 22:42:33 -0500

    2nd required question's input saves & redisplays properly - g2g

--- Boolean Logic (Project - Insurance Approval)/Program.cs
+++ Boolean Logic (Project - Insurance Approval)/Program.cs
@@ -10,6 +10,10 @@ namespace Boolean_Logic__Project___Insurance_Approval_
             string usersAge = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> int' conversion...
             int age = Convert.ToInt32(usersAge); // converting this number to a more appropriate data type
 
+            Console.WriteLine("(Please answer with either \"True\" or \"False\")\nHave you ever had a DUI?"); // program's 2nd required question
+            string anyDUIs = Console.ReadLine(); // User's input returns as a string; saving to a variable for 'string -> boolean' conversion...
+            bool DUIs = Convert.ToBoolean(anyDUIs); // converting the string its more accurate data type
+
             Console.ReadLine();
         }
     }
