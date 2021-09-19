e5aad28195684a8119135a9ec6631221815f05cc
DForeman <dlydellf@hotmail.com>
2021-09-19 00:04:27 -0500

    Created one-dimen array of strings

---
+++ Console App Pt1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Pt1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] stringArray = { "This is String0", "This is String1", "This is String2", "This is String3", "This is String4", "This is String5", "This is String6" };
+
+        }
+    }
+}
