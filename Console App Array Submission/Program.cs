b3430c8d2327a5054ca3f79f5bfaa1923aace88b
DForeman <dlydellf@hotmail.com>
2021-09-18 19:56:24 -0500

    User's chosen number displays string from array's correct index

---
+++ Console App Array Submission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_App_Array_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            // An array of strings, assigned to a string variable ("stringArray"):
+            string[] stringArray = { "Hello world", "Hi, world!", "Hello, Planet Earth!", "Hello there, Earthling", "Status 404: string not found" };
+            
+            // Console displays this instruction to User:
+            Console.WriteLine("Please select a number (0-4) to display the accompanying string from the array: ");
+            // User's chosen number is captured, conveerted from a string into an int, & assigned to an int variable ("choice"):
+            int choice = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(stringArray[choice]); // Array's string (at User's chosen index) is displayed to the console
+            Console.ReadLine(); // console remains opened for viewing
+        }
+    }
+}
