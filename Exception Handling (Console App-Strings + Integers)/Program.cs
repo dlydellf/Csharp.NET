f1ddd23f9595ccff4d72ef50d4c411132cafedd6
DForeman <dlydellf@hotmail.com>
2021-09-24 12:00:37 -0500

    'Foreach' loop divides each dividend by User's divisor & displays quotient

---
+++ Exception Handling (Console App-Strings + Integers)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exception_Handling__Console_App_Strings___Integers_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> dividends = new List<int>{ 100, 200, 300, 400, 500 };
+            Console.WriteLine("Please enter a number to divide each integer by: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            foreach (int dividend in dividends)
+            {
+                int quotient = dividend / divisor;
+                Console.WriteLine($"{dividend} divided by {divisor} = {quotient}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
