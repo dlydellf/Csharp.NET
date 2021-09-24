aa48995577b748e44513bac4dfa62f6e483d3bb5
DForeman <dlydellf@hotmail.com>
2021-09-24 12:13:54 -0500

    Added comments to code

--- Exception Handling (Console App-Strings + Integers)/Program.cs
+++ Exception Handling (Console App-Strings + Integers)/Program.cs
@@ -7,13 +7,15 @@ namespace Exception_Handling__Console_App_Strings___Integers_
     {
         static void Main(string[] args)
         {
-            List<int> dividends = new List<int>{ 100, 200, 300, 400, 500 };
-            Console.WriteLine("Please enter a number to divide each integer by: ");
+            // Creates required list of integers (List<int>); list is named "dividends":
+            List<int> dividends = new List<int> { 100, 200, 300, 400, 500 };
+            Console.WriteLine("Please enter a number to divide each integer by: "); // User's displayed instructions
+            //User's input returns a <string>; converts that into a <int>, & assigns to variable "divisor"
             int divisor = Convert.ToInt32(Console.ReadLine());
-            foreach (int dividend in dividends)
+            foreach (int dividend in dividends) // foreach numb in <list>...
             {
-                int quotient = dividend / divisor;
-                Console.WriteLine($"{dividend} divided by {divisor} = {quotient}");
+                int quotient = dividend / divisor; //...divide by User input, assign answer to 'quotient'
+                Console.WriteLine($"{dividend} divided by {divisor} = {quotient}"); // display results to console
             }
             Console.ReadLine();
         }
