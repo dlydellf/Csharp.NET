d12f6df036dd6672f567fcdfd1d90b1347d3a76f
DForeman <dlydellf@hotmail.com>
2021-09-25 18:07:15 -0500

    User input saved to an int variable

---
+++ CallingMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to perform math operations on?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
