8d5393f0ee734d1227954ed9e0d327dda0e2390c
DForeman <dlydellf@hotmail.com>
2021-09-25 19:27:09 -0500

    Added comments to code - submitting

--- CallingMethods/Program.cs
+++ CallingMethods/Program.cs
@@ -13,17 +13,26 @@ namespace CallingMethods
             try
             {
                 Console.WriteLine("What number would you like to perform math operations on?");
-                int number = Convert.ToInt32(Console.ReadLine());
+                int number = Convert.ToInt32(Console.ReadLine()); // User's input converts to dataType <int>
 
                 Math_Operations.Adding(number); // calls class's 1st method
                 Math_Operations.Multiply(number); // calls class's 2nd method
                 Math_Operations.Divide(number); // calls class's 3rd method
                 Console.WriteLine("\nYour chosen number has successfully been passed into three seperate methods as their input!");
             }
+            catch (FormatException) // catches any "formatting" exceptions
+            {
+                Console.WriteLine("Please enter a whole number, no text nor decimals"); // displays this error messaage
+            }
+            catch (Exception ex) // catches any OTHER exceptions (Overflow, etc.)
+            {
+                Console.WriteLine(ex.Message); // displays this error message
+            }
             finally
             {
-                Console.ReadLine();
+                Console.ReadLine(); // this ALWAYS runs, no matter what
             }
+
         }
     }
 }
