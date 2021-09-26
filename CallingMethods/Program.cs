2ed5f108cff5dd1bf6ce391977f5af79a055bd00
DForeman <dlydellf@hotmail.com>
2021-09-25 19:26:12 -0500

    Called each method; results display to console

--- CallingMethods/Program.cs
+++ CallingMethods/Program.cs
@@ -14,6 +14,15 @@ namespace CallingMethods
             {
                 Console.WriteLine("What number would you like to perform math operations on?");
                 int number = Convert.ToInt32(Console.ReadLine());
+
+                Math_Operations.Adding(number); // calls class's 1st method
+                Math_Operations.Multiply(number); // calls class's 2nd method
+                Math_Operations.Divide(number); // calls class's 3rd method
+                Console.WriteLine("\nYour chosen number has successfully been passed into three seperate methods as their input!");
+            }
+            finally
+            {
+                Console.ReadLine();
             }
         }
     }
