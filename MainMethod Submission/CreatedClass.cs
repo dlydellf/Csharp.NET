f1c0ae5892b3749c5a75f6ac6cc67f8b96ca503b
DForeman <dlydellf@hotmail.com>
2021-09-27 17:20:04 -0500

    Created 1st class; accepts an <int>, adds to it, & returns answer as another <int>

---
+++ MainMethod Submission/CreatedClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod_Submission
+{
+    class CreatedClass
+    {
+        // 1st required class: accepts (and adds) an <int> dataType, rreturning the answer as an <int> ("sum"):
+        public static int firstMethod(int numb1)
+        {
+            int sum = numb1 + 10; // addition is performed
+            Console.WriteLine($"{numb1} + 10 = {sum}"); // displays result to console
+            Console.ReadLine(); // console stays opened until User closes it
+            return sum;
+        }
+    }
+}
