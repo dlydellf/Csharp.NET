29eaa92e4a4ea40513d83302065a2d7b2f0fcb2f
DForeman <dlydellf@hotmail.com>
2021-09-27 19:13:39 -0500

    Created a class accepting 2 <int>s (one is optional); returns an <int>

---
+++ Method Submission/CreatedClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/* Commenting-out these unused Namespaces:
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Method_Submission
+{
+    // Required class:
+    class CreatedClass
+    {
+        // Required method; accepts 2 <int>s, does a calculation, & returns an <int>:
+        public static int createdMethod(int numb1, int numb2 = 4321) // "numb1" is User input, "numb2" is optional
+        {
+            int product = numb1 * numb2; // calculation
+            Console.WriteLine($"{numb1} x {numb2} = {product}"); // its result
+            Console.ReadLine(); // keeps console open for User's viewing
+            return product; // method must return an <int>
+        }
+    }
+}
---
+++ Method Submission/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatedClass.createdMethod(1234); // instantiating the class
+        }
+    }
+}
