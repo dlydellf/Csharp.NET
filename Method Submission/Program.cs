7701e0fbef3e322aae1d811bfa68692355216324
DForeman <dlydellf@hotmail.com>
2021-09-27 19:20:47 -0500

    User asked for 2 nums, one at a time (2nd num optional).

--- Method Submission/Program.cs
+++ Method Submission/Program.cs
@@ -10,7 +10,11 @@ namespace Method_Submission
     {
         static void Main(string[] args)
         {
-            CreatedClass.createdMethod(1234); // instantiating the class
+            Console.WriteLine("Please enter a number:\nNumber 1 = ....");
+            int numb1 = Convert.ToInt32(Console.ReadLine()); // Converts & assigns User's 1st input to <int> "numb1"
+            Console.WriteLine("(Optional) Please enter a 2nd number:\nNumber 2 = ...");
+            int numb2 = Convert.ToInt32(Console.ReadLine()); // converts & assigns User's 2nd input to <int> "numb2"
+            CreatedClass.createdMethod(numb1, numb2); // instantiating the class
         }
     }
 }
