43e967651df7213b44085e5297bdbb5edde7471b
DForeman <dlydellf@hotmail.com>
2021-09-30 19:28:28 -0500

    Created required class & its method

---
+++ MethodClass Submission/CreatedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+/* Commenting-out these unused NameSpaces:
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace MethodClass_Submission
+{
+    // Required class:
+    class CreatedClass
+    {
+        // Required method; accepts two <int> parameters, does math to the 1st & displays the 2nd:
+        public void createdMethod(int num1, int num2) { }
+    }
+}
