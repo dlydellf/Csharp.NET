ad52284d067f37299f41f210a67be3ea3bfdb991
DForeman <dlydellf@hotmail.com>
2021-10-01 17:45:41 -0500

    Created required class & its void method

---
+++ Class Submission/CreatedClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/* Commenting-out these unused NameSpaces:
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Class_Submission
+{
+    // Required class; accepts an <int>, divides it by two, and outputs an <int>:
+    class CreatedClass
+    {
+        public void createdMethod(int num1)
+        {
+            int result = num1 / 2;
+        }
+    }
+}
