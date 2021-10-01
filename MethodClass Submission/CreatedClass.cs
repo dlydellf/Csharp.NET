1a4e5cbe8c71352551aca81b7a854f3e41e10db1
DForeman <dlydellf@hotmail.com>
2021-09-30 19:34:33 -0500

    Added functionality to class's method

--- MethodClass Submission/CreatedClass.cs
+++ MethodClass Submission/CreatedClass.cs
@@ -11,6 +11,11 @@ namespace MethodClass_Submission
     class CreatedClass
     {
         // Required method; accepts two <int> parameters, does math to the 1st & displays the 2nd:
-        public void createdMethod(int num1, int num2) { }
+        public void createdMethod(int num1, int num2)
+        {
+            _ = num1 * 2021; // Math is done to 1st parameter, although resulkt doesn't need to be displayed
+            Console.WriteLine($"The 1st parameter, {num1}\nwas multiplied by the current year (2021).\nThe 2nd parameter is being displayed here: {num2}");
+            Console.ReadLine();
+        }
     }
 }
