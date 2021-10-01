1a80bfc0b5829db6d869def1d32cb28f671bea2d
DForeman <dlydellf@hotmail.com>
2021-10-01 18:17:22 -0500

    User asked for a number; it's divided by 2 & output to console.

--- Class Submission/CreatedClass.cs
+++ Class Submission/CreatedClass.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;*/
 
 namespace Class_Submission
 {
-    // Required class; accepts an <int>, divides it by two, and outputs an <int>:
+    // Required class:
     class CreatedClass
     {
+        // Required method; accepts an <int>, divides it by two, and outputs an <int>:
         public void createdMethod(int num1)
         {
             int result = num1 / 2;
+            Console.WriteLine($"Your entered numbeer, {num1}, divided by 2\nequals {result}");
+            Console.ReadLine(); // keeps console opened for User's viewing
         }
     }
 }
--- Class Submission/Program.cs
+++ Class Submission/Program.cs
@@ -12,6 +12,11 @@ namespace Class_Submission
         static void Main(string[] args)
         {
             CreatedClass instantiatedClass = new CreatedClass(); // instantiates an instance of the class
+
+            Console.WriteLine("Please enter a number"); // User is asked to enter a number
+            int num1 = Convert.ToInt32(Console.ReadLine()); // User's input is converted from a <string> to an <int>
+
+            instantiatedClass.createdMethod(num1); // method is called with User's input
         }
     }
 }
