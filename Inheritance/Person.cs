6986ab59e6ab3c1b2936370d4f4ae131541f8bbb
DForeman <dlydellf@hotmail.com>
2021-10-03 12:26:26 -0500

    Created class & its 2 <string> properties

---
+++ Inheritance/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+/* Commenting-out these unused NameSpaces:
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Inheritance
+{
+    // Required class:
+    class Person
+    {
+        // Required <string> properties:
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
