2c18b728900f9de96446de6b13e5892dae3ef900
DForeman <dlydellf@hotmail.com>
2021-10-03 12:32:19 -0500

    Created "Employee" class, which inherites from "Person" class

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+/* Commenting-out these unused NameSpaces:
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace Inheritance
+{
+    // Required class, inheriting from class "Person":
+    class Employee : Person
+    {
+        // Required property:
+        public int Id { get; set; }
+    }
+}
