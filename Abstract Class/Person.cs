029a575bd87216db79b7f6b003e67587abdd8b9b
DForeman <dlydellf@hotmail.com>
2021-10-03 22:19:12 -0500

    Created required abstract class w/both required properties

---
+++ Abstract Class/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    // Required abstract class:
+    public abstract class Person
+    {
+        // Required properties:
+        string firstName { get; set; }
+        string lastName { get; set; }
+
+        // Required method:
+        public void SayName()
+        {
+
+        }
+    }
+}
