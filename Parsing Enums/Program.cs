cfe241f9499032a96ef20f1262baaad9389f4a26
DForeman <dlydellf@hotmail.com>
2021-10-15 13:39:45 -0400

    Created an enum of the days of the week

---
+++ Parsing Enums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
