d291ab392d93509df91bcc402fdd76dd76c71649
DForeman <dlydellf@hotmail.com>
2021-10-15 16:58:08 -0400

    Created <struct> w/<decimal> property

---
+++ Struct Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        // created <struct> (named "Number") w/<decimal> property (called "Amount"):
+        struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
