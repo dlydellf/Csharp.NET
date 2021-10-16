14e7fa80ab42d0acfa0002c9b54a330581d79369
DForeman <dlydellf@hotmail.com>
2021-10-16 14:03:29 -0400

    Created <List> of employees w/1st & last names & Ids; 2 employees have same 1st name

---
+++ Lambda Expressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>();
+            employees.Add("01 - Joe Adams");
+            employees.Add("02 - Joe Biden");
+            employees.Add("03 - Douglas Foreman");
+            employees.Add("04 - Employee Four");
+            employees.Add("05 - Employee Five");
+            employees.Add("06 - Employee Six");
+            employees.Add("07 - Employee Seven");
+            employees.Add("08 - Employee Eight");
+            employees.Add("09 - Employee Nine");
+            employees.Add("10 - Employee Ten");
+
+            Console.WriteLine(employees.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
