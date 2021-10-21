7c43d3c79fd9b706c6d2da01efe8776276737c78
DForeman <dlydellf@hotmail.com>
2021-10-21 14:56:33 -0400

    Prints current DateTime to console

---
+++ DateTime Submission Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This prints the current date & time to the console:
+            Console.WriteLine($"This is the current date & time:\n{DateTime.Now}");
+            Console.WriteLine("\n(press Enter)");
+            Console.ReadLine();
+        }
+    }
+}
