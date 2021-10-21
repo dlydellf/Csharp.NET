bb6d3b642f06279f84925bc4bfc0a8971bfabbfa
DForeman <dlydellf@hotmail.com>
2021-10-21 15:02:19 -0400

    Asks the User for a number

--- DateTime Submission Assignment/Program.cs
+++ DateTime Submission Assignment/Program.cs
@@ -14,6 +14,22 @@ namespace DateTime_Submission_Assignment
             Console.WriteLine($"This is the current date & time:\n{DateTime.Now}");
             Console.WriteLine("\n(press Enter)");
             Console.ReadLine();
+
+            // Asking the User for a number:
+            Console.WriteLine("Please enter a number:");
+            try
+            {
+                int UsersNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(UsersNumber);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter only numbers");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
