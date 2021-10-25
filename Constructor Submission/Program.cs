d68219d6133b9de2d1800e00acc4db5d08edf220
DForeman <dlydellf@hotmail.com>
2021-10-25 13:37:57 -0400

    Created a variable using the keyword "var"

--- Constructor Submission/Program.cs
+++ Constructor Submission/Program.cs
@@ -14,6 +14,12 @@ namespace Constructor_Submission
             const string myName = "Douglas";  // my 1st name is assigned to a <string> variable (named "myName"); since my 1st name will never change, the variable is also a <constant>
             Console.WriteLine($"My first name ({myName}) has been assigned to the constant variable \"myName\".");
             Console.ReadLine();
+
+            // Required <var> variable:
+            string declaration = "This is a random <string>, assigned to a implicitly-defined variable";
+            var randomString = ($"{declaration} (using \"var\", like this:)\nvar randomString = {declaration}.");  // since the dataType (<string>) is obvious, the keyword "var" replaces the dataType's keyword ("string")
+            Console.WriteLine($"{randomString}");
+            Console.ReadLine();
         }
     }
 }
