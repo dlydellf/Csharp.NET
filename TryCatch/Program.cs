8fec7a6069585c6b0189a8ccadc9ff6c22eb97d0
DForeman <dlydellf@hotmail.com>
2021-10-27 15:09:47 -0400

    Calculates & displays User's year of birth

--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -11,8 +11,9 @@ namespace TryCatch
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your age:");  // User is asked for their age
-            int age = Convert.ToInt32(Console.ReadLine());  // Converts User's age from a <string> & assins the value to the <int> variable "age"
-            Console.WriteLine($"Your age was recorded as: {age}");
+            int age = Convert.ToInt32(Console.ReadLine());  // converts User's age from a <string> & assigns the value to the <int> variable "age"
+            int birthYear = DateTime.Today.Year - age;  // assigns (current year - User's age) to the <int> variable "birthYear"
+            Console.WriteLine($"Your year of birth was: {birthYear}");  // displays User's year of birth to the console
             Console.ReadLine();
         }
     }
