e03a9ced622e29602c795e8820e177eaccfff4fe
DForeman <dlydellf@hotmail.com>
2021-10-27 15:19:17 -0400

    Exceptions are handled using "try/catch"

--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -11,10 +11,20 @@ namespace TryCatch
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your age:");  // User is asked for their age
-            int age = Convert.ToInt32(Console.ReadLine());  // converts User's age from a <string> & assigns the value to the <int> variable "age"
-            int birthYear = DateTime.Today.Year - age;  // assigns (current year - User's age) to the <int> variable "birthYear"
-            Console.WriteLine($"Your year of birth was: {birthYear}");  // displays User's year of birth to the console
-            Console.ReadLine();
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());  // converts User's age from a <string> & assigns the value to the <int> variable "age"
+                int birthYear = DateTime.Today.Year - age;  // assigns (current year - User's age) to the <int> variable "birthYear"
+                Console.WriteLine($"Your year of birth was: {birthYear}");  // displays User's year of birth to the console
+            }
+            catch (Exception)  // Any exceptions are handled from this catch block:
+            {
+                Console.WriteLine("An error occurred - please contact your System Administrator.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
