bb2a3b422bc1754ad093be2fd83234a7da32e941
DForeman <dlydellf@hotmail.com>
2021-10-27 15:34:17 -0400

    Age <= 0 throws SPECIFIC exception; other exceptions have general message

---
+++ TryCatch/ImpossibleException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    public class ImpossibleException : Exception
+    {
+        public ImpossibleException()  // a standard CONSTRUCTOR
+            : base() { }  // INHERITS from the base's ("Exception") CONSTRUCTOR
+        public ImpossibleException(string message)  //"overloading" the above constrcutor's method, takes a <string> argument
+            : base(message) { }  // inherits from the base ("Exception")'s CONSTRUCTOR, which ALSO has a constrcutor taking a <string> argument
+    }
+}
--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -14,16 +14,27 @@ namespace TryCatch
             try
             {
                 int age = Convert.ToInt32(Console.ReadLine());  // converts User's age from a <string> & assigns the value to the <int> variable "age"
-                int birthYear = DateTime.Today.Year - age;  // assigns (current year - User's age) to the <int> variable "birthYear"
-                Console.WriteLine($"Your year of birth was: {birthYear}");  // displays User's year of birth to the console
+                if (age > 0)  // as long as User's age is greater than zero...
+                { //... this code block executes.
+                    int birthYear = DateTime.Today.Year - age;  // assigns (current year - User's age) to the <int> variable "birthYear"
+                    Console.WriteLine($"Your year of birth was: {birthYear}");  // displays User's year of birth to the console
+                }
+                else  // if User's age is LESS THAN (or EQUAL TO) zero...
+                {
+                    throw new ImpossibleException();  //... this SPECIFIC exceotion is thrown/created (Line 27)
+                }                
             }
-            catch (Exception)  // Any exceptions are handled from this catch block:
+            catch (ImpossibleException)
+            {
+                Console.WriteLine("Your age cannot be zero or below.");
+            }
+            catch (Exception)  // All other exceptions (Format, Overflow, etc.) arethrow this GENERAL exception:
             {
                 Console.WriteLine("An error occurred - please contact your System Administrator.");
             }
             finally
             {
-                Console.ReadLine();
+                Console.ReadLine();  // console remains opened for viewing
             }
         }
     }
